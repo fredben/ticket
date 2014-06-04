2e24ffde15394fda4db578ca983aec53c49310de
freddy <fred_ben01@outlook.com>
2014-06-04 16:06:05 -0500

    Mensajes de error

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,10 +14,11 @@ class ProjectsController < ApplicationController
     @project = Project.new(project_params)
 
     if @project.save
-      flash[:notice] = "Project has been created"
+      flash[:notice] = "Project has been created."
       redirect_to @project
     else
-      #nothing,yet
+      flash[:alert] = "Project has not been created."
+      render "new"
     end
   end
 
